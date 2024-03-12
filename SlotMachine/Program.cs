aa910567bcfd05fa95ea4e23adaa460857513a89
Von <samuel.washington@snhu.edu>
2024-03-12 03:04:10 -0700

    Defined constants for magic numbers. Added validation for user inputted game selection and wager.

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -6,8 +6,9 @@ namespace SlotMachine
     {
         static void Main(string[] args)
         {
-            const int NDX_0 = 0, GAME_1 = 1, NDX_1 = 1, NDX_2 = 2, GAME_3 = 3, ROW_COLUMN_LENGTH = 3, NUM_CHECKS_2 = 2, NUM_CHECKS_3 = 3, NUM_CHECKS_5 = 5, NUM_CHECKS_6 = 6;
-            const decimal HALF_BONUS = 0.50M;
+            const int ROW_COLUMN_LENGTH = 3, NDX_0 = 0,  NDX_1 = 1, NDX_2 = 2, NDX_3 = 3, GAME_1 = 1, GAME_2 = 2, GAME_3 = 3, GAME_4 = 4, GAME_5 = 5;
+            const int A_DOLLAR = 1, NUM_CHECKS_2 = 2, NUM_CHECKS_3 = 3, NUM_CHECKS_5 = 5, NUM_CHECKS_6 = 6, NUM_CHECKS_7 = 7, NUM_CHECKS_8 = 8;
+            const decimal A_CENT = 0.01M, HALF_BONUS = 0.50M, PLAYER_MONEY_RESET = 20.00M;
             const string WIN = "WIN!", LOST = "LOST!";
             decimal playerMoney = 20.00M;
             Random rng = new Random();
@@ -18,12 +19,12 @@ namespace SlotMachine
             while (true)
             {
                 // Get Random Numbers for Slot Machine
-                int[,] slotMachine = new int[3,3];
+                int[,] slotMachine = new int[NDX_3,NDX_3];
                 for (int i = 0; i < ROW_COLUMN_LENGTH; i++)
                 {
                     for (int j = 0; j < ROW_COLUMN_LENGTH; j++)
                     {
-                        slotMachine[i, j] = rng.Next(3);
+                        slotMachine[i, j] = rng.Next(NDX_3);
                     }
                 }
 
@@ -31,37 +32,58 @@ namespace SlotMachine
                 Console.WriteLine("\nSelect gameplay: ");
                 Console.WriteLine("1. Center line");
                 Console.WriteLine("2. All horizontal lines");
-                Console.WriteLine("3. All vertical and diagonal lines\n");
+                Console.WriteLine("3. All vertical lines\n");
+                //Console.WriteLine("4. All diagonal lines");
+                //Console.WriteLine("5. All lines (horizontal, vertical, and diagonal)\n");
+
+                int number;
+                int userInputGameSelection = 0;
 
                 // Take Player Game Selection
                 Console.Write("Play Game ");
-                int userInputNumLines = int.Parse(Console.ReadLine());
-
-                // Repeat Player Game Selection if selection is out of bound.
-                while (userInputNumLines < GAME_1 || userInputNumLines > GAME_3)
+                if (int.TryParse(Console.ReadLine(), out number))
                 {
-                    Console.Write("Play Game ");
-                    userInputNumLines = int.Parse(Console.ReadLine());
+                    userInputGameSelection = number;
                 }
 
+                // Repeat Player Game Selection if selection is out of bound.
+                while (userInputGameSelection < GAME_1 || userInputGameSelection > GAME_3)
+                {
+                    Console.Write("Play Game ");
+                    if (int.TryParse(Console.ReadLine(), out number))
+                    {
+                        userInputGameSelection = number;
+                    }
+                }
+
+                decimal amount;
+                decimal userInputWager = 0;
                 // Take Player Wager
                 Console.WriteLine($"\nPlayer money: {playerMoney:F2}");
                 Console.Write($"How much is your wager (cannot exceed amount of player money): ");
-                decimal userInputWager = decimal.Parse(Console.ReadLine());
+                
+                if (decimal.TryParse(Console.ReadLine(), out amount)) 
+                {
+                    userInputWager = amount;
+                }
 
                 // Repeat Player Wager if wager is out of bound.
-                while (userInputWager < 0 || userInputWager > playerMoney)
+                while (userInputWager < A_CENT || userInputWager > playerMoney)
                 {
                     Console.WriteLine($"\nPlayer money: {playerMoney:F2}");
                     Console.Write($"How much is your wager (cannot exceed amount of player money): ");
-                    userInputWager = decimal.Parse(Console.ReadLine());
+                    if (decimal.TryParse(Console.ReadLine(), out amount))
+                    {
+                        userInputWager = amount;
+                    }
+
                 }
 
                 // Set Amounts
                 playerMoney -= userInputWager;
-                decimal potentialWinAmount = HALF_BONUS * (userInputWager) + 1;
-                int numChecks = (userInputNumLines == GAME_3) ? 8 : 3;
-                potentialWinAmount += (userInputNumLines == GAME_3) ? (userInputWager / 5) - 1: (userInputWager / 3) - 1;
+                decimal potentialWinAmount = HALF_BONUS * (userInputWager) + A_DOLLAR;
+                int numChecks = (userInputGameSelection == GAME_3) ? NUM_CHECKS_8 : NUM_CHECKS_3;
+                potentialWinAmount += (userInputGameSelection == GAME_3) ? (userInputWager / NUM_CHECKS_5) - A_DOLLAR: (userInputWager / NUM_CHECKS_3) - A_DOLLAR;
                 Console.WriteLine($"Potential Win Amount per line: {potentialWinAmount:F2}"); // DELETE CODE
 
                 // Print Slot Machine Roll
@@ -83,6 +105,7 @@ namespace SlotMachine
                 int[] row_col_diag = { 0, 0, 0 };
                 int ndxCounter = 0;
 
+
                 // Check Lines
                 while (ndxCounter < numChecks) {
 
@@ -94,7 +117,7 @@ namespace SlotMachine
                         for (int i = 0; i < ROW_COLUMN_LENGTH; i++)
                         {
                             // ERROR - Rework    
-                            if (userInputNumLines == GAME_1 && ndxCounter != NDX_1) 
+                            if (userInputGameSelection == GAME_1 && ndxCounter != NDX_1) 
                             {
                                 continue;
                             }
@@ -108,7 +131,7 @@ namespace SlotMachine
                     {
                         for (int i = 0; i < ROW_COLUMN_LENGTH; i++)
                         {
-                            row_col_diag[i] = slotMachine[i, ndxCounter - 3];
+                            row_col_diag[i] = slotMachine[i, ndxCounter - NDX_3];
                         }
                     }
                    
@@ -118,14 +141,14 @@ namespace SlotMachine
                     {
                         for (int i = 0; i < ROW_COLUMN_LENGTH; i++)
                         {
-                            if (ndxCounter == 6)
+                            if (ndxCounter == NUM_CHECKS_6)
                             {
                                 row_col_diag[i] = slotMachine[i, i];
                             }
                             
-                            if (ndxCounter == 7)
+                            if (ndxCounter == NUM_CHECKS_7)
                             {
-                                for (int j = 2; j >= 0; j--)
+                                for (int j = NDX_2; j >= 0; j--)
                                 {
                                     row_col_diag[i] = slotMachine[i, j];
                                 }
@@ -133,11 +156,11 @@ namespace SlotMachine
                         }
                     }
 
-                    // Determine if row is a win.
+                    // Determine if row is a win. //
                     int rowChecker = 0;
-                    if (!(userInputNumLines == GAME_1 && ndxCounter != NDX_1))
+                    if (!(userInputGameSelection == GAME_1 && ndxCounter != NDX_1))
                     {
-                        for (int i = 1; i < ROW_COLUMN_LENGTH; i++)
+                        for (int i = NDX_1; i < ROW_COLUMN_LENGTH; i++)
                         {
                             if (row_col_diag[NDX_0] == row_col_diag[i])
                             {
@@ -145,7 +168,7 @@ namespace SlotMachine
                             }
                         }
 
-                        if (rowChecker == 2)
+                        if (rowChecker == NUM_CHECKS_2)
                         {
                             winAmount += potentialWinAmount;
                             Win = true;
@@ -193,7 +216,7 @@ namespace SlotMachine
                 if (playerMoney == 0) 
                 {
                     Console.WriteLine("\nPlayer has lost all money. Refreshing game...\n");
-                    playerMoney = 20.00M;
+                    playerMoney = PLAYER_MONEY_RESET;
                 }
             }
         }
